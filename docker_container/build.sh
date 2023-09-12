1adf39067b500ac9649efed2133723d8d882a46e
khinggan <1516113201@qq.com>
2023-09-12 10:32:33 +0900

    Modify focalfoxyNWM Dockerfile
    Add commbot:khinggan to cornet docker

--- docker_container/build.sh
+++ docker_container/build.sh
@@ -2,4 +2,4 @@
 
 docker build -t cornet:focalfoxyNWH -f Dockerfile.focalfoxyNWH .
 # docker build -t cornet:ubuntu1804 -f Dockerfile.ubuntu1804 .
-docker build -t commbot:khinggan -f Dockerfile.khinggan .
+# docker build -t commbot:khinggan -f Dockerfile.khinggan .
